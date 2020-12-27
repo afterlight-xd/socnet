16c7f124dea14a0fb212a8c42ee8aaad1e71df92
afterlight <afterlight055@gmail.com>
2020-12-27 10:10:40 +0700

    forgot to actually commit additional html files xd

--- src/main/java/v7x/socnet/config/SpringSecurityConfig.java
+++ src/main/java/v7x/socnet/config/SpringSecurityConfig.java
@@ -32,20 +32,22 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/").permitAll()
-                    .anyRequest()
-                    .authenticated()
-                .and()
-                    .formLogin()
-                    .loginPage("/auth/login").permitAll()
-                    .defaultSuccessUrl("/auth/success")
-                .and()
-                    .logout()
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout", "POST"))
-                    .invalidateHttpSession(true)
-                    .clearAuthentication(true)
-                    .deleteCookies("JSESSIONID")
-                    .logoutSuccessUrl("/auth/login");
+                        .antMatchers("/index").permitAll()
+                        .antMatchers("/register").permitAll()
+                        //.antMatchers("/users").permitAll()
+                        .anyRequest()
+                        .authenticated()
+                    .and()
+                        .formLogin()
+                        .loginPage("/auth/login").permitAll()
+                        .defaultSuccessUrl("/auth/success")
+                    .and()
+                        .logout()
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout", "POST"))
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID")
+                        .logoutSuccessUrl("/auth/login");
     }
 
     @Override
--- src/main/java/v7x/socnet/model/Role.java
+++ src/main/java/v7x/socnet/model/Role.java
@@ -6,8 +6,8 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public enum Role {
-    USER(Set.of(Permission.DEVELOPERS_READ)),
-    ADMIN(Set.of(Permission.DEVELOPERS_READ, Permission.DEVELOPERS_WRITE));
+    ROLE_USER(Set.of(Permission.DEVELOPERS_READ)),
+    ROLE_ADMIN(Set.of(Permission.DEVELOPERS_READ, Permission.DEVELOPERS_WRITE));
 
     private final Set<Permission> permissions;
 
